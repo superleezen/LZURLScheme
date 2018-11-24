37e5f7a4dfa1f1846d673a6ea580a12e7ddba424
lizheng <lizheng@limikeji.com>
2018-11-24 13:37:11 +0800

    Initial commit

---
+++ Example/LZURLScheme/LZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LZAppDelegate.h
+//  LZURLScheme
+//
+//  Created by emailoflizheng@126.com on 11/24/2018.
+//  Copyright (c) 2018 emailoflizheng@126.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LZURLScheme/LZViewController.m
@@ -0,0 +1,29 @@
+//
+//  LZViewController.m
+//  LZURLScheme
+//
+//  Created by emailoflizheng@126.com on 11/24/2018.
+//  Copyright (c) 2018 emailoflizheng@126.com. All rights reserved.
+//
+
+#import "LZViewController.h"
+
+@interface LZViewController ()
+
+@end
+
+@implementation LZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
